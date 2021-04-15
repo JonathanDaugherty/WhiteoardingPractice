5e7d4543a756f210e677daf9523c8942e481123e
jmdaugherty94@gmail.com <jmdaugherty94@gmail>
2021-04-15 10:59:26 -0400

    Add project files.

---
+++ WhiteoardingPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteoardingPractice {
+    class Program {
+        static void Main(string[] args) {
+            //var total = 0;
+            //for (var idx = 1; idx < 51; idx++)
+            //    if (idx % 7 == 0 || idx % 5 == 0) {
+            //        total += idx;
+            //    }
+            //Console.WriteLine($"Total is {total}");
+            //Console.ReadKey();
+
+
+            // Fibonacci Series
+            //int a = 1;
+            //int b = 1;
+            //int c = 0;
+            //while (c <= 76) {
+            //    c = a + b;
+            //    a = b;
+            //    b = c;
+            // }
+
+
+            var scores = new int[] { 4, 73, 67, 38, 33 };
+            var rounded = new int[5];
+            for (var idx = 0; idx < scores.Length; idx++) {
+                rounded[idx] = scores[idx];
+                if (scores[idx] < 38) continue;
+                var inc = 0;
+                while((scores[idx] + inc) % 5 != 0) {
+                    inc++;
+                }
+                if(inc <= 2) {
+                    rounded[idx] += inc;
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
